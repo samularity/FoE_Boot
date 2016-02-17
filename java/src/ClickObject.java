565be2e735c68d51c948d53bee45a95af9692428
janitz <janis-d@web.de>
2016-02-17 10:52:47 +0100

    1000000 changes everything is ClickObject instead of BufferedImage now

--- java/src/ClickObject.java
+++ java/src/ClickObject.java
@@ -1,4 +1,7 @@
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by Janis on 04.02.2016.
@@ -12,6 +15,7 @@ public class ClickObject {
 
     //name of the element for the log
     public String imgName;
+    public String path;
 
     //is it allowed to click a second of this element directly after the first
     public boolean multipleAllowed;
@@ -19,13 +23,17 @@ public class ClickObject {
     //click offset
     public int xOffset;
     public int yOffset;
+    public int maxClickTolerance;
 
     //constructor
-    ClickObject(BufferedImage img, String imgName, boolean multipleAllowed, int xOffset, int yOffset){
-        this.img = img;
+    ClickObject(String path, String imgName, boolean multipleAllowed, int xOffset, int yOffset, int maxClickTolerance ,Log log) throws IOException{
+        log.print("load: " + imgName, Log.LOGLEVEL.DEBUG);
+        this.img = ImageIO.read(new File(path + imgName));
+        this.path = path;
         this.imgName = imgName;
         this.multipleAllowed = multipleAllowed;
         this.xOffset = xOffset;
         this.yOffset = yOffset;
+        this.maxClickTolerance = maxClickTolerance;
     }
 }
