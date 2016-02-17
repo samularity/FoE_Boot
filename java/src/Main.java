c2dd1250907607be8a9056c97c25f42daeeee293
samularity <samuelmunz@gmx.de>
2016-02-17 14:38:58 +0100

    add little mouse move in every loop

--- java/src/Main.java
+++ java/src/Main.java
@@ -167,7 +167,8 @@ public class Main {
                 }
 
                 doClose(bot, close, close1, close2, close3, clickArea);
-
+                Point mouseLocation = MouseInfo.getPointerInfo().getLocation();
+                bot.mouseMove(mouseLocation.x+10,mouseLocation.y+10);
             }
 
             //catch all exceptions and print them...
